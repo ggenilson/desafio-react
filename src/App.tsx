05c057253c9f0ed4e26a8a61f7042d0e30d79a3a
ggenilson <genilsonaraujo20002002@gmail.com>
2021-06-26 03:37:58 +0100

    feat: criação do componente para cadastro e estilização do formulário e container para cadastro de candidato

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { FC } from 'react';
 import './styles/index.css';
-import Input from './components/input';
-
-import store from './assets/img/cadastro.png';
-import gria from './assets/img/gria.png';
+import StoreCandidate from './pages/candidate';
 
 const App: FC = () => {
   return (
@@ -12,30 +9,7 @@ const App: FC = () => {
         <span>Retornar</span>
       </div>
 
-      <div className="container-form">
-        <div className="left-img">
-          <img src={store} alt="Cadastro" />
-        </div>
-
-        <form>
-          <div className="header-form">
-            <div className="left-header-form">
-              <h2>Cadastrar Candidato</h2>
-              <span>Cadastre-se e encontre a oportunidade que deseja</span>
-            </div>
-            <div className="right-header-form">
-              <img src={gria} alt="Gria" />
-            </div>
-          </div>
-
-          <div className="body-form">
-            <Input type="text" id="cpf" name="cpf" />
-            <Input type="email" id="email" name="email" />
-            <Input type="password" id="password" name="password" />
-            <Input type="password" id="password1" name="password1" />
-          </div>
-        </form>
-      </div>
+      <StoreCandidate />
     </div>
   );
 };
